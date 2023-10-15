67bed228992838feec7314748a747a944524bf7d
Aidan McBride <aidankmcbride@gmail.com>
2023-10-15 19:48:17 -0400

    cleanup functionality in app

--- src/App.js
+++ src/App.js
@@ -1,96 +1,174 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 //Components
-import LandinPage from './components/LandinPage'
-import WeatherPage from './components/WeatherPage'
-import WatchClass from './components/WatchClass'
+import LandinPage from './components/LandinPage';
+import WeatherPage from './components/WeatherPage';
+import WatchClass from './components/WatchClass';
 
 function App() {
 
-  const [show, setShow] = useState(false)
-  const [weather, setWeather] = useState({
-    //farenheit
-    temperature: '',
-    humidity: '',
-    feelsLike: '',
-    windSpeed: '',
-    windDirection: '',
-    //in milliseconds
-    sunrise: '',
-    sunset: '',
-    //visibility in meters
-    visibility: '',
-    //perc clouds
-    cloudy: '',
-    main: '',
-    description: '',
-    city: ''
-  })
+    const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
+    const ip2LocationApiKey = process.env.REACT_APP_IP2_LOCATION_API_KEY;
+    const isLocalOrDev = process.env.NODE_ENV || 'development';
+    const localTestIP = process.env.REACT_APP_LOCAL_TEST_IP;
+    const defaultZipCodeNYC = '10001';
 
-  //fetch to weather API
-  const handleSubmit = (e, zipCode) => {
-    e.preventDefault()
-    const apiKey = `03b80556af981e9f9a6a57906ecf2438`
-    console.log(zipCode)
-    const numbers = /^[0-9]+$/
-    if (zipCode.length === 5 && zipCode.match(numbers)) {
-      console.log("success")
+    console.log('Env: ', process.env.NODE_ENV);
 
-      fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&units=imperial&appid=${apiKey}`)
+    const [errorMessage, setErrorMessage] = React.useState('');
+    const [weather, setWeather] = React.useState({
+        //farenheit
+        temperature: '',
+        humidity: '',
+        feelsLike: '',
+        windSpeed: '',
+        windDirection: '',
+        //in milliseconds
+        sunrise: '',
+        sunset: '',
+        //visibility in meters
+        visibility: '',
+        // % clouds
+        cloudy: '',
+        main: '',
+        description: '',
+        city: ''
+    });
+
+    React.useEffect(() => {
+        init();
+    }, []);
+
+    async function init() {
+        let ip;
+        if (isLocalOrDev) {
+            ip = localTestIP;
+        } else {
+            ip = await getUserIp();
+        }
+        const localData = await getDataFromIp(ip);
+        let zip = localData?.zip_code || defaultZipCodeNYC;
+        const weather = await getWeatherByZipCode(zip);
+        if (weather) {
+            parseWeatherDataToState(weather);
+        }
+    }
+
+    async function getUserIp() {
+        return fetch('https://geolocation-db.com/json/')
+            .then(resp => resp.json())
+            .then(response => response.IPv4)
+            .catch((err) => {
+                console.error("Failed to get the user's IP address. Error Message: ", err)
+            });
+    }
+
+    async function getDataFromIp(ipAddress) {
+        return fetch(`https://api.ip2location.io/?key=${ip2LocationApiKey}&ip=${ipAddress}`)
+            .then(resp => resp.json())
+            .then(response => response)
+            // Returns:   
+            // {     
+            //      "ip": string Ex: 99.999.999.999
+            //      "country_code": string Ex: "US"
+            //      "country_name": string Ex: "United States of America",
+            //      "region_name": string Ex: "North Carolina",
+            //      "city_name": string Ex: "Rahway",
+            //      "latitude": numner Ex: 40.60821,
+            //      "longitude": number Ex: -74.27755,
+            //      "zip_code": string Ex: "12345",
+            //      "time_zone": string Ex: "-04:00",
+            //      "asn": string Ex: "9999",
+            //      "as": string Ex: "Comcast Cable Communications LLC",
+            //      "is_proxy": boolean Ex: false
+            //  }
+            .catch((err) => {
+                console.error('Failed to get Browser Data from IP address. Error Message: ', err);
+            });
+    }
+
+    async function getWeatherByZipCode(zipCode) {
+        return fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&units=imperial&appid=${weatherApiKey}`)
         .then(resp => resp.json())
         .then(weather => {
-          console.log("this:", weather.cod)
-          if (weather.cod !== "404") {
-            setShow(false)
-            setWeather({
-              //farenheit
-              city: weather.name,
-              temperature: Math.round(weather.main.temp * 10) / 10,
-              humidity: weather.main.humidity,
-              feelsLike: Math.round(weather.main.feels_like * 10) / 10,
-              windSpeed: weather.wind.speed,
-              windDirection: weather.wind.deg,
-              //in milliseconds
-              sunrise: weather.sys.sunrise,
-              sunset: weather.sys.sunset,
-              //visibility in meters
-              visibility: weather.visibility,
-              cloudy: weather.clouds.all,
-              main: weather.weather[0].main,
-              description: weather.weather[0].description
-            })
-          }
+            if (weather.cod !== "404") {
+                return weather;
+            }
         })
-    } else {
-      console.log("invalid input")
-      setShow(true)
-      setWeather({ city: "" })
+        .catch((err) => {
+            console.error('Failed to get the weather from api.openweathermap.org. Error Message: ', err);
+        });
     }
-  }
 
-  return (
-    <div className="App">
-      <div class="heading" >
-        <br />
-        <h1 style={{
-          fontSize: "58px",
-          color: "white",
-          textShadow: "1.5px 1.5px black",
-          backgroundColor: "rgba(0,0,0,0.10)",
-          borderRadius: "25px",
-          width: "40%",
-          margin: "auto",
-          border: "1px solid black"
-        }}>Weather App</h1>
-      </div>
-      <LandinPage handleSubmit={handleSubmit} />
-      <WatchClass />
-      {show ?
-        <div style={{ display: "inline-block", borderRadius: "25px" }} class="alert alert-danger" role="alert">Please Enter a 5 Digit Numeric Zip Code!</div> : null}
-      {weather.city !== "" ? <WeatherPage weather={weather} /> : null}
-    </div>
-  );
-}
+    const handleSubmit = async (e, zipCode) => {
+        e.preventDefault()
+        const numbers = /^[0-9]+$/;
+        if (zipCode.length === 5 && zipCode.match(numbers)) {
+            const weather = await getWeatherByZipCode(zipCode);
+            if (weather) {
+                setErrorMessage('');
+                parseWeatherDataToState(weather);
+            } else {
+                setErrorMessage('Unable to find the weather for that zip code. Please be sure to enter a valid 5 digit numeric zip code');
+            }
+        } else {
+            setErrorMessage('Unable to find the weather for that zip code. Please be sure to enter a valid 5 digit numeric zip code');
+        }
+    }
+
+    function parseWeatherDataToState(weatherData) {
+        setWeather({
+            //farenheit
+            city: weatherData.name,
+            temperature: Math.round(weatherData.main.temp * 10) / 10,
+            humidity: weatherData.main.humidity,
+            feelsLike: Math.round(weatherData.main.feels_like * 10) / 10,
+            windSpeed: weatherData.wind.speed,
+            windDirection: weatherData.wind.deg,
+            //in milliseconds
+            sunrise: weatherData.sys.sunrise,
+            sunset: weatherData.sys.sunset,
+            //visibility in meters
+            visibility: weatherData.visibility,
+            cloudy: weatherData.clouds.all,
+            main: weatherData.weather[0].main,
+            description: weatherData.weather[0].description
+        })
+    }
+
+    return (
+        <div className="App">
+            <div className="heading" >
+                <br />
+                <h1 style={{
+                    fontSize: "58px",
+                    color: "white",
+                    textShadow: "1.5px 1.5px black",
+                    backgroundColor: "rgba(0,0,0,0.10)",
+                    borderRadius: "25px",
+                    width: "40%",
+                    margin: "auto",
+                    border: "1px solid black"
+                }}>
+                    Weather App
+                </h1>
+            </div>
+            <LandinPage handleSubmit={handleSubmit} />
+            <WatchClass />
+            {errorMessage.length > 0 &&
+                <div style={{ display: "inline-block", borderRadius: "25px" }} className="alert alert-danger" role="alert">
+                    {errorMessage}
+                </div> 
+            }
+            {
+                ( weather && weather.city !== "") ? 
+                    <WeatherPage weather={weather} /> 
+                : 
+                    <span>What's the weather like?</span>
+            }
+        </div>
+    );
+};
 
 export default App;
--- src/components/WatchClass.js
+++ src/components/WatchClass.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 class WatchClass extends Component {
+
     constructor(props) {
         super(props);
         //This declared the state of time at the very beginning
         this.state = {
             time: new Date().toLocaleTimeString()
         }
-    }
+    };
+
     //This happens when the component mount and 
     //the setInterval function get called with
     // a call back function updateClock()
@@ -15,26 +17,29 @@ class WatchClass extends Component {
             this.updateClock(),
             1000
         );
-    }
+    };
+    
     //This section clears setInterval by calling
     // intervalID so as to optimise memory
     componentWillUnmount() {
         clearInterval(this.intervalID)
-    }
+    };
+
     //This function set the state of the time to a new time
     updateClock() {
         this.setState({
             time: new Date().toLocaleTimeString()
         });
-    }
+    };
+
     render() {
         return (
-            <div class="float-right-top" style={{ position: "absolute", right: "0", top: "0", marginRight: "10px" }}>
+            <div className="float-right-top" style={{ position: "absolute", right: "0", top: "0", marginRight: "10px" }}>
                 <h3
                     style={{ color: "White", textShadow: "1px 1px black" }}>
                     {this.state.time}</h3>
             </div>
         );
-    }
-}
+    };
+};
 export default WatchClass;
